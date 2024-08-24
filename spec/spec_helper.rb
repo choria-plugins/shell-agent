e063a50a0c5bb3e5651219d826104428f147e4e9
Romain Tartière <romain@blogreen.org>
2024-08-24 18:19:01 +0200

    Use the RSpec 1 syntax with `should`
    The test suite use the RSpec 1 syntax with `should` that got deprecated.
    In order to ease up the transition, for now we want to stick with the
    `should` syntax, and *not* use `expect`.
    
    While here, also ensure exceptions are raised when deprecated features
    are used.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,8 @@ require 'mcollective/test'
 RSpec.configure do |config|
   config.mock_with :mocha
   config.include(MCollective::Test::Matchers)
+  config.raise_errors_for_deprecations!
+  config.expect_with(:rspec) { |c| c.syntax = :should }
 
   config.before :each do
     MCollective::PluginManager.clear
